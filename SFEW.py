33e8ad09ed955e6e6e3dec62f21b0bdfacdac85a
tkurtovic98 <tkurtovic98@gmail.com>
2023-11-22 17:24:08 +0100

    Add support for SFEW dataset.

---
+++ SFEW.py
@@ -0,0 +1,49 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+def prepare_dataset(batch_size: int = 32):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Resize to the input size of the model
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    train_dataset = SFEW('SFEW/train', transform=transform)
+    val_dataset = SFEW('SFEW/val', transform=transform)
+    # test_dataset = SFEW('SFEW/test', transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    # test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader, None
+
+
+class SFEW(Dataset):
+    def __init__(self, directory, transform=None):
+        self.directory = directory
+        self.transform = transform
+        self.classes = os.listdir(directory)
+        self.images = []
+        self.labels = []
+
+        for index, label in enumerate(self.classes):
+            class_path = os.path.join(directory, label)
+            for img_file in os.listdir(class_path):
+                self.images.append(os.path.join(class_path, img_file))
+                self.labels.append(index)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image_path = self.images[idx]
+        image = Image.open(image_path).convert('RGB')
+        label = self.labels[idx]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
---
+++ mainpro_SFEW.py
@@ -0,0 +1,74 @@
+import os
+import torch
+from torch import nn, optim
+
+from mainpro_FER import parse_args
+from checkpoint import set_checkpoint_path, load_checkpoint
+from models import get_model
+from training_state import TrainingState
+from learning import LearningRateDecay, run_validation, train
+from plot_progress import plot_progress
+from SFEW import prepare_dataset
+
+TOTAL_EPOCH = 250
+
+if __name__ == "__main__":
+    opt = parse_args()
+    
+    assert opt.dataset == "SFEW", "Dataset should be SFEW"
+    
+    opt = parse_args()
+
+    path = os.path.join(opt.dataset + '_' + opt.model)
+
+    set_checkpoint_path(path)
+
+    net = get_model(opt.model)
+
+    state = TrainingState()
+
+    use_cuda = torch.cuda.is_available()
+
+    if opt.resume:
+        # Load checkpoint.
+        checkpoint = load_checkpoint('PrivateTest_model.t7')
+
+        net.load_state_dict(checkpoint.net)
+
+        state.best_PrivateTest_acc = checkpoint.best_val_acc
+        state.best_PrivateTest_acc_epoch = checkpoint.best_val_epoch
+
+        state.best_PublicTest_acc = checkpoint.best_test_acc
+        state.best_PublicTest_acc_epoch = checkpoint.best_val_epoch
+
+        start_epoch = checkpoint.best_val_epoch + 1
+    else:
+        print('==> Building model..')
+        start_epoch = 0
+
+    if use_cuda:
+        net.cuda()
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=opt.lr, momentum=0.9, weight_decay=5e-4)
+    learning_rate = opt.lr
+    learning_rate_decay = LearningRateDecay(start = 100, every=5, rate=0.9)
+
+    train_set_loader, test_set_loader, validation_set_loader = prepare_dataset(opt.bs)
+    
+    for epoch in range(start_epoch, TOTAL_EPOCH):
+        state.set_epoch(epoch)
+        state = train(epoch, state, learning_rate_decay, net, train_set_loader, learning_rate, optimizer, loss_fn)
+        # state = run_testing(epoch, state, net, test_set_loader, learning_rate, optimizer, loss_fn)
+        state = run_validation(epoch, state, net, validation_set_loader, learning_rate, optimizer, loss_fn)
+        plot_progress(state)
+
+    best_public_test_acc, best_public_test_epoch = state.get_best_PublicTest_acc()
+    best_private_test_acc, best_private_test_epoch = state.get_best_PrivateTest_acc()
+
+    print(
+        f"Best Test Accuracy: {best_public_test_acc} at Epoch {best_public_test_epoch}")
+    print(
+        f"Best Validation Test Accuracy: {best_private_test_acc} at Epoch {best_private_test_epoch}")
+
+    
