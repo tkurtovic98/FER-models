df8d011f6688c98f759f29051e99a6403aee400c
tkurtovic98 <tkurtovic98@gmail.com>
2023-11-22 16:23:34 +0100

    Fix make dir bug.

--- checkpoint.py
+++ checkpoint.py
@@ -22,7 +22,7 @@ def load_checkpoint(name:str) -> Checkpoint:
 def save_checkpoint(name: str, checkpoint: Checkpoint):
     assert checkpoint_path != '', 'Error: checkpoint path not set!'
     if not os.path.isdir(checkpoint_path):
-        os.mkdir(checkpoint_path)
+        os.makedirs(checkpoint_path, 0o755, True)
     torch.save(asdict(checkpoint), os.path.join(checkpoint_path, name))    
 
 checkpoint_path = ''
